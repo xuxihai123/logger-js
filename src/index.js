af73a25bc452a2bc7762d28261fcceb0687ae632
xuxihai <xuxihai@360.net>
2018-12-29 14:13:43 +0800

    es6 write

--- src/index.js
+++ src/index.js
@@ -1,79 +1,69 @@
-function Logger(options) {
-  options = options || {};
-  this._name = options.name || '';
-  this._level = options.level || 0;  //%t %n  %l
-  this._format = options.format || '[%t] %l:';  //%t %n  %l
-}
-
-Logger.prototype._logger = function(level, ...args) {
-  if (['log', 'info', 'debug', 'warn', 'error'].indexOf(level) >= this._level) {
-    let timestrap = new Date().toTimeString().
+class Logger{
+  constructor(options={}){
+    this._name = options.name || '';
+    this._level = options.level || 0;  //%t %n  %l
+    this._format = options.format || '[%t] %l:';  //%t %n  %l
+  }
+  log(...args){
+    this._logger('log', ...args);
+  }
+  info(...args){
+    this._logger('info', ...args);
+  }
+  debug(...args){
+    this._logger('debug', ...args);
+  }
+  warn(...args){
+    this._logger('warn', ...args);
+  }
+  error(...args){
+    this._logger('error', ...args);
+  }
+  _logger(type,...args){
+    if (['log', 'info', 'debug', 'warn', 'error'].indexOf(type) >= this._level) {
+      let timestrap = new Date().toTimeString().
         replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
-    let prefix;
-    level = level.toUpperCase();
-    let options = { name: this._name, timestrap, level };
-    if (typeof this._format === 'function') {
-      prefix = this._format(options);
-    } else {
-      prefix = this._formatMsg(options, this._format);
-    }
-    try {
-      console[level].apply(console, [prefix, ...args]);
-    } catch (e) {
-      console.log.apply(console, [prefix, ...args]);
+      let prefix;
+      let level = type.toUpperCase();
+      let options = { name: this._name, timestrap, level };
+      if (typeof this._format === 'function') {
+        prefix = this._format(options);
+      } else {
+        prefix = this._formatMsg(options, this._format);
+      }
+      try {
+        console[type].apply(console, [prefix, ...args]);
+      } catch (e) {
+        console.log.apply(console, [prefix, ...args]);
+      }
     }
   }
-};
-
-Logger.prototype._formatMsg = function(options, format) {
-  return format.replace(/%t/, options.timestrap).
+  _formatMsg(options,format){
+    return format.replace(/%t/, options.timestrap).
       replace(/%l/, options.level).
       replace(/%n/, options.name);
-};
-
-Logger.prototype.log = function(msg = '', ...args) { //0
-  this._logger.apply(this, ['log', msg, ...args]);
-};
-
-Logger.prototype.info = function(...args) {  //1
-  this._logger.apply(this, ['info', ...args]);
-};
-
-Logger.prototype.debug = function(...args) { //2
-  this._logger.apply(this, ['debug', ...args]);
-};
-
-Logger.prototype.warn = function(...args) { //3
-  this._logger.apply(this, ['warn', ...args]);
-};
-
-Logger.prototype.error = function(...args) { //4
-  this._logger.apply(this, ['error', ...args]);
-};
-
-Logger.prototype.setFormat = function(_format) { //set format
-  this._format = _format;
-  return this;
-};
-
-Logger.prototype.setScope = function(name) { //set scope
-
-  this._name = name;
-  return this;
-};
-
-Logger.prototype.setLevel = function(level) { //set level
-  this._level = level;
-  return this;
-};
-Logger.prototype.set=function(key,value) {
-  if(['name','level','format'].indexOf(key)!==-1){
-    this['_' + key] = value;
-    return this;
-  }else{
-    throw Error('key '+key+' is not exist');
   }
-};
+  setFormat(_format){
+    this._format = _format;
+    return this;
+  }
+  setScope(name){
+    this._name = name;
+    return this;
+  }
+  setLevel(level){
+    this._level = level;
+    return this;
+  }
+  set(key,value){
+    if(['name','level','format'].indexOf(key)!==-1){
+      this['_' + key] = value;
+      return this;
+    }else{
+      throw Error('key '+key+' is not exist');
+    }
+  }
+}
 
 let globalLogger = new Logger();
 globalLogger.scope = function(name) {
